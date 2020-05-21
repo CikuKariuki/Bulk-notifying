b3be31803fd022dfb7db1f9705c2e704d8db7486
Wanjiku <wanjikukariuki03@gmail.com>
2020-05-22 00:23:29 +0300

    Sent sms to array with 2 numbers

--- src/notification-actions/sms-action.js
+++ src/notification-actions/sms-action.js
@@ -9,12 +9,14 @@ export default function makeSmsSender({ makeAxios }) {
     async function startSendSMS({ ...messageInfo }) {
         // messageInfo is the body, merchant_id and message
         const axios = await makeAxios()
-        var array = ["254729616980","254775604548"]
+        var array = ["254776776096","254727766302"]
         // for (i=array[0]; i<=array.length; i++){}
+        array.forEach(function phone(item, index, arr){
+            item = arr[index]
         const data = {
             "api_key": process.env.smsApiKey,
             "service_id": 0,
-            "mobile": replacewithphonefromarray, //input array of numbers from contactsdb look how notifications looped through, but first how csvitems was ooped through.
+            "mobile": item, //input array of numbers from contactsdb look how notifications looped through, but first how csvitems was ooped through.
             "response_type": "json",
             "shortcode": "Tilil",
             "message": messageInfo.message
@@ -27,5 +29,6 @@ export default function makeSmsSender({ makeAxios }) {
         .catch((error)=>{
             console.log(error)
         })
+    })
     }
 }
