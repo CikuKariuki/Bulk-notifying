57c423de8b2c942bf924d8dd13104a655b3f943b
Wanjiku <wanjikukariuki03@gmail.com>
2020-05-22 20:23:50 +0300

    successful upload, next send sms

--- src/controllers/post-uploads.js
+++ src/controllers/post-uploads.js
@@ -1,22 +1,31 @@
-import response from './http-response'
-
-export default function createPostUpload({ addUpload }) {
-    return async function postUpload(uploadInfo) {
-        try {
-            // const merchant_id = httpRequest.params.merchant_id
-            // const group_id = httpRequest.body.group_id
-            const posted = await addUpload({ ...uploadInfo })
-            return response.httpResponse(posted)
-
+export default function createPostUpload ({ addUpload }){
+    return async function postUpload (uploadInfo){
+        try{
+            const posted = await addUpload ({
+                ...uploadInfo
+            })
+            return {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                statusCode: 200,
+                body: {
+                    "status": 200,
+                    "message": "Contacts have successfully been uploaded"
+                }
+            }
         }
-        catch(e){
-            const errorBody = {
-                status: 400,
-                message: {
+        catch (e){
+            console.log("In post-deals", e)
+            return {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                statusCode: 400,
+                body: {
                     error: e.message
                 }
             }
-            return response.httpResponse(errorBody)
         }
     }
 }
